38d443f6ac4c92c51e80c218cd3036383bfa36fb
ediunnes <ediunnes@gmail.com>
2025-05-11 23:52:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tombolSapa = document.getElementById('tombolSapa');
+
+    if (tombolSapa) {
+        tombolSapa.addEventListener('click', function() {
+            alert('Marilah kita bacakan Al Fatihah untuk Bapak, Ibu, dan Mbak Erni');
+        });
+    }
+});
