d95698939d14ae972dca6c71c3b90158e056dbeb
ediunnes <ediunnes@gmail.com>
2025-05-12 21:12:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (tombolSapa) {
         tombolSapa.addEventListener('click', function() {
-            alert('Marilah kita bacakan Al Fatihah untuk Bapak, Ibu, dan Mbak Erni');
+            alert('Marilah kita bacakan Al Fatihah untuk Bapak, Ibu, dan Mbak Erni بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِیْمِ
+اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِیْنَ(1) الرَّحْمٰنِ الرَّحِیْمِ(2) مٰلِكِ یَوْمِ الدِّیْنِﭤ(3) اِیَّاكَ نَعْبُدُ وَ اِیَّاكَ نَسْتَعِیْنُﭤ(4) اِهْدِنَا الصِّرَاطَ الْمُسْتَقِیْمَ(5) صِرَاطَ الَّذِیْنَ اَنْعَمْتَ عَلَیْهِمْ ﴰ غَیْرِ الْمَغْضُوْبِ عَلَیْهِمْ وَ لَا الضَّآلِّیْنَ(7)');
         });
     }
 });
